cedd3a42842faf0535222b72fb4ea692ab03eb2e
Rodion94 <63346271+Rodion94@users.noreply.github.com>
2026-02-28 11:17:53 +0700

    5.6 Работа с авторизацией в Playwright Практика работы с авторизацией в Playwright Add playwright_courses.py

--- playwright_courses.py
+++ playwright_courses.py
@@ -48,5 +48,25 @@ with sync_playwright() as playwright:
 
     """Открываем новую страницу в рамках контекста"""
     page = context.new_page()
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
-    page.wait_for_timeout(5000)
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+    """Проверяем наличие и текст заголовка "Courses"""
+    courses_text = page.get_by_test_id('courses-list-toolbar-title-text')
+    expect(courses_text).to_be_visible()
+    expect(courses_text).to_have_text('Courses')
+
+    """Проверяем наличие и видимость иконки пустого блока"""
+    empty_block_icon = page.get_by_test_id('courses-list-empty-view-icon')
+    expect(empty_block_icon).to_be_visible()
+
+    """Проверяем наличие и текст блока "There is no results"""
+    no_results_text = page.get_by_test_id('courses-list-empty-view-title-text')
+    expect(no_results_text).to_be_visible()
+    expect(no_results_text).to_have_text('There is no results')
+
+    """Проверяем наличие и текст описания блока: "Results from the load test pipeline will be displayed here"""
+    block_description_text = page.get_by_test_id('courses-list-empty-view-description-text')
+    expect(block_description_text).to_be_visible()
+    expect(block_description_text).to_have_text('Results from the load test pipeline will be displayed here')
+
+    page.wait_for_timeout(3000)
