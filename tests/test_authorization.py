a2d79564fe2f3f67caf5fe0d130a8597b6812255
Rodion94 <63346271+Rodion94@users.noreply.github.com>
2026-03-13 08:41:58 +0700

    7.1 Знакомство с PageObject Практика работы с Page Object Add POM for test_registration.py

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,6 +1,5 @@
 import pytest
-from playwright.sync_api import expect, Page
-
+from pages.login_page import LoginPage
 
 
 @pytest.mark.regression
@@ -10,25 +9,16 @@ from playwright.sync_api import expect, Page
     ("user.name@gmail.com", "  "),
     ("  ", "password")
 ])
-# Использование фикстуры 'chromium_page', которая автоматически предоставляет готовую страницу
-def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
-    # Теперь страница передаётся в тест через фикстуру 'chromium_page', браузер не нужно инициализировать вручную
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+# Использование фикстуры 'login_page', которая автоматически предоставляет готовую страницу
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
+    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
-    """Заполняем поле Email"""
-    email_input = chromium_page.get_by_test_id('login-form-email-input').locator('input')
-    email_input.fill(email)
+    # Заполняем форму авторизации
+    login_page.fill_login_form(email=email, password=password)
+    # Нажимаем кнопку "Login"
+    login_page.click_login_button()
+    # Проверяем наличие сообщения об ошибке
+    login_page.click_login_button()
+    login_page.check_visible_wrong_email_or_password_alert()
 
-    """Заполняем поле Password"""
-    password_input = chromium_page.get_by_test_id('login-form-password-input').locator('input')
-    password_input.fill(password)
-
-    """Нажимаем на кнопку Login"""
-    login_button = chromium_page.get_by_test_id('login-page-login-button')
-    login_button.click()
-
-    """Проверяем, что появляется сообщение об ошибке"""
-    wrong_email_or_password_alert = chromium_page.get_by_test_id('login-page-wrong-email-or-password-alert')
-    expect(wrong_email_or_password_alert).to_be_visible()
-    expect(wrong_email_or_password_alert).to_have_text('Wrong email or password')
 
